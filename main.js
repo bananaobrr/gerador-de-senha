aecbdc8d71c92cab2b52b3b049171f376f78a37b
bananaobrr <142402700+bananaobrr@users.noreply.github.com>
2025-07-04 08:01:52 -0300

    fffffffffff

--- main.js
+++ main.js
@@ -62,10 +62,10 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha();
+    classificaSenha(alfabeto.length);
 }
-    function classificaSenha(){
-    let entropia = tamanhoSenha * Math.log2(alfabeto.length); 
+    function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoalfabeto); 
     console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
 
